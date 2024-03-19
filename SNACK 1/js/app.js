c09ab42fc57bc405065adec3dbf3cca0734af6fa
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-19 18:30:32 +0100

    ADD: added the same solution using the method map on the snack 1

--- SNACK 1/js/app.js
+++ SNACK 1/js/app.js
@@ -2,21 +2,37 @@ const tavoloVip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo'
 
 //generiamo un array vuoto dove poi andremo a pushare dentro i vari oggetti che avranno come proprietà
 //nomeOspite = tavolovip[i]
-const segnaposto = []
+// const segnaposto = []
 
 //creo un ciclo for per iterare ogni elemento dell'array tavoloVip
 
-for(let i = 0; i < tavoloVip.length; i++){
-    const membroVip = tavoloVip[i]
-    console.log(membroVip)
+// for(let i = 0; i < tavoloVip.length; i++){
+//     const membroVip = tavoloVip[i]
+    
 
-    //pusho all'interno del'array segnaposto i vari oggetti utilizzando come nome, la variabile membroVip
-    segnaposto.push(
-    {
-        nomeOspite: `${membroVip}`,
+//     //pusho all'interno del'array segnaposto i vari oggetti utilizzando come nome, la variabile membroVip
+//     segnaposto.push(
+//     {
+//         nomeOspite: `${membroVip}`,
+//         nomeTavolo: "Tavolo VIP",
+//         posto: i + 1
+//     },
+//     )
+// }
+// console.log(segnaposto)
+
+
+//-------
+
+//altra soluzione utilizzando il metodo map
+
+const segnaposti = tavoloVip.map((el, i,) => {
+    const segnaposto = {
+        nomeOspite: el,
         nomeTavolo: "Tavolo VIP",
         posto: i + 1
-    },
-    )
-}
-console.log(segnaposto)
+    }
+    return segnaposto
+})
+
+console.log(segnaposti)
