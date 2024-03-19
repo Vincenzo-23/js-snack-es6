7a68df9599849e0d85615636fa9685e569fa0d17
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-19 14:34:10 +0100

    ADD: added the js logic for the snack 1

--- SNACK 1/js/app.js
+++ SNACK 1/js/app.js
@@ -1,1 +1,21 @@
-console.log("Inizio")
+const tavoloVip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
+
+//generiamo un array vuoto dove poi andremo a pushare dentro i vari oggetti che avranno come proprietà
+//nomeOspite = tavolovip[i]
+const segnaposto = []
+
+//creo un ciclo for per iterare ogni elemento dell'array tavoloVip
+
+for(let i = 0; i < tavoloVip.length; i++){
+    const membroVip = tavoloVip[i]
+    console.log(membroVip)
+
+    segnaposto.push(
+    {
+        nomeOspite: `${membroVip}`,
+        nomeTavolo: "Tavolo VIP",
+        posto: i + 1
+    },
+    )
+}
+console.log(segnaposto)
